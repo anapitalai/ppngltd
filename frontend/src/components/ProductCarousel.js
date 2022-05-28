17921ed7fba2a3e32077af22d7753e1d4fca368f
Alois Napitalai <anapitalai@gmail.com>
2022-05-28 18:01:19 +1000

    fix carousel

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -4,32 +4,29 @@ import { Carousel, Image } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from './Loader'
 import Message from './Message'
-import { listTopProducts } from '../actions/productActions'
+// import { listTopProducts } from '../actions/productActions'
+
+const products=[{"name":"Realestate","image":"land.png"},{"name":"Realestate","image":"industrial.png"},{"name":"Realestate","image":"residential.png"}]
 
 const ProductCarousel = () => {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-  const productTopRated = useSelector((state) => state.productTopRated)
-  const { loading, error, products } = productTopRated
+  // const productTopRated = useSelector((state) => state.productTopRated)
+  // const { loading, error, products } = productTopRated
 
-  useEffect(() => {
-    dispatch(listTopProducts())
-  }, [dispatch])
+  // useEffect(() => {
+  //   dispatch(products)
+  // }, [dispatch])
 
-  return loading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant='danger'>{error}</Message>
-  ) : (
-    <Carousel pause='hover' className='bg-light'>
+  return (
+    <Carousel pause='hover' className='bg-myred'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
               <h2>
-                {product.name} 
-                {/* (${product.price}) */}
+                {product.name} (${product.price})
               </h2>
             </Carousel.Caption>
           </Link>
